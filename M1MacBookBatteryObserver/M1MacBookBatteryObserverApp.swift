6747a8171a9774e27cb28100f3b83374e30655aa
Shingo Fukuyama <houseshou@gmail.com>
2022-02-24 01:20:31 +0900

    Initial Commit

---
+++ M1MacBookBatteryObserver/M1MacBookBatteryObserverApp.swift
@@ -0,0 +1,17 @@
+//
+//  M1MacBookBatteryObserverApp.swift
+//  M1MacBookBatteryObserver
+//
+//  Created by Shingo Fukuyama on 2022/02/24.
+//
+
+import SwiftUI
+
+@main
+struct M1MacBookBatteryObserverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
